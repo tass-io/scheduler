2630ae8e813f111ddb9f3a41863b32ee55c84de5
ty0207 <ty0207@sjtu.edu.cn>
2021-05-19 04:24:40 +0000

    fix error about config mkdir

--- test/request.go
+++ test/request.go
@@ -2,19 +2,18 @@ package test
 
 import (
 	"encoding/json"
-	"fmt"
-	"go.uber.org/zap"
 	"io/ioutil"
 	"net/http"
 	"strings"
+
+	"go.uber.org/zap"
 )
 
 // wrapper request to local scheduler
 func RequestJson(url string, method string, headers map[string]string, param interface{}, bodyStruct interface{}) (int, error) {
 	var jsonByte []byte
 	jsonByte, _ = json.Marshal(param)
-	client := &http.Client{
-	}
+	client := &http.Client{}
 	req, err := http.NewRequest(method, url, strings.NewReader(string(jsonByte)))
 
 	if err != nil {
@@ -24,11 +23,10 @@ func RequestJson(url string, method string, headers map[string]string, param int
 	for key, val := range headers {
 		req.Header.Add(key, val)
 	}
-	fmt.Println(req)
 
 	res, err := client.Do(req)
 	if err != nil {
-		return 0,err
+		return 0, err
 	}
 	defer res.Body.Close()
 
