6a0e6ece5700eb523d6a94f39a7f260dc0b55f2e
ty0207 <ty0207@sjtu.edu.cn>
2021-05-30 16:29:15 +0000

    add demo for prom

--- pkg/prom/prom.go
+++ pkg/prom/prom.go
@@ -1,6 +1,14 @@
 package prom
 
+import "github.com/prometheus/client_golang/prometheus"
+
+var Demo = prometheus.NewGauge(prometheus.GaugeOpts{
+	Name:        "demo",
+	Help:        "demo var for tass",
+	ConstLabels: nil,
+})
+
 // run before route define, now at root.go
 func init() {
-	
+	_ = prometheus.Register(Demo)
 }
